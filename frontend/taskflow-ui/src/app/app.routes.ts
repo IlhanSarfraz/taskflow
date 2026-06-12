f8cb582796eba25d7aa9b1e32995b3aff11b6fa6
Ilhan Sarfraz <ilhansarfraz45@gmail.com>
2026-06-12 12:36:27 +0500

    feat(auth): add route guard for protecting authenticated routes

--- frontend/taskflow-ui/src/app/app.routes.ts
+++ frontend/taskflow-ui/src/app/app.routes.ts
@@ -1,39 +1,48 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'projects',
     pathMatch: 'full'
   },
+
+  {
+    path: 'projects',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/projects/pages/project-list/project-list.component')
+        .then(m => m.ProjectListComponent)
+  },
+
+  {
+    path: 'projects/create',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/projects/pages/create-project/create-project.component')
+        .then(m => m.CreateProjectComponent)
+  },
+
+  {
+    path: 'projects/edit/:id',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/projects/pages/edit-project/edit-project.component')
+        .then(m => m.EditProjectComponent)
+  },
+
   {
     path: 'login',
     loadComponent: () =>
       import('./features/auth/pages/login/login.component')
         .then(m => m.LoginComponent)
   },
+
   {
     path: 'register',
     loadComponent: () =>
       import('./features/auth/pages/register/register.component')
         .then(m => m.RegisterComponent)
-  },
-  {
-    path: 'projects',
-    loadComponent: () =>
-      import('./features/projects/pages/project-list/project-list.component')
-        .then(m => m.ProjectListComponent)
-  },
-  {
-    path: 'projects/create',
-    loadComponent: () =>
-      import('./features/projects/pages/create-project/create-project.component')
-        .then(m => m.CreateProjectComponent)
-  },
-  {
-    path: 'projects/edit/:id',
-    loadComponent: () =>
-      import('./features/projects/pages/edit-project/edit-project.component')
-        .then(m => m.EditProjectComponent)
   }
 ];
